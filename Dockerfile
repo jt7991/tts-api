c4e4cb0a3427b665868413d5a377084db2a491cf
Jacob Traunero <jt7991@gmail.com>
2024-09-27 20:27:15 -0400

    minio setup

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,8 @@ COPY requirements.txt requirements.txt
 
 RUN apt-get update && apt-get install -y git
 RUN pip install -r requirements.txt
+RUN python -m unidic download
 
 COPY . .
 
-CMD [ "python", "-m", "unidic", "download" ]
 CMD [ "gunicorn", "-w", "4", "-b", "0.0.0.0:8000", "main:app" ]
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from melo.api import TTS
+import os
+import uuid
 from flask import request
 from flask import Flask
+from minio import Minio, S3Error
+from dotenv import load_dotenv
+
 import nltk
 nltk.download('averaged_perceptron_tagger_eng')
 
@@ -15,13 +20,32 @@ device = 'auto' # Will automatically use GPU if available
 model = TTS(language='EN', device=device)
 speaker_ids = model.hps.data.spk2id
 
-output_path = 'en-us.wav'
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def process_text():
+    minio_url = os.environ.get('MINIO_URL')
+    minio_access_key = os.environ.get('MINIO_ACCESS_KEY')
+    minio_secret_key = os.environ.get('MINIO_SECRET_KEY')
+
+    if not minio_url or not minio_access_key or not minio_secret_key:
+        return "Please set MINIO_URL, MINIO_ACCESS_KEY, and MINIO_SECRET_KEY environment variables."
+
+    minio_client = Minio(minio_url,
+        access_key= minio_access_key,
+        secret_key= minio_secret_key,
+    )
+
+    uuid_str = str(uuid.uuid4())
     text = request.get_json()["text"]
-    print(text)
-    model.tts_to_file(text, speaker_ids['EN-Default'], output_path, speed=speed)
-    return output_path
+    
+
+    output_path_minio = uuid_str + '.wav'
+    model.tts_to_file(text, speaker_ids['EN-Default'], output_path_minio, speed=speed)
+    minio_client.fput_object('tts', output_path_minio, output_path_minio)
+
+    # Generate the file URL
+    file_url = minio_client.presigned_get_object("tts", output_path_minio)
+    return file_url
