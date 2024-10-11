ebf2a4b381f8556acef96eb248d7dc276a94fcb7
jt7991 <jt7991@gmail.com>
2024-10-10 21:35:03 -0400

    fuzz

--- backend/site_parser.py
+++ backend/site_parser.py
@@ -3,7 +3,7 @@ import requests
 def parse_site(url): 
     content = requests.get(url)
     soup = BeautifulSoup(content.text, 'html.parser')
-    article = soup.find('article') or soup.find('section') or soup.find('div')
+    article = soup.find('article') or soup.find('main') or soup.find('section') or soup.find('div')
     text = ''
     if article:
        text = article.get_text(strip=True)
