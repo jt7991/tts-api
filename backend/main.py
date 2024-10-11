5c65e0f76477a3977ade104e769ff05b5e294a81
jt7991 <jt7991@gmail.com>
2024-10-10 21:11:52 -0400

    wip

--- backend/main.py
+++ backend/main.py
@@ -6,9 +6,22 @@ from flask import render_template
 from flask import Flask
 from minio import Minio, S3Error
 from dotenv import load_dotenv
-import nltk
 from site_parser import parse_site
 from tts import tts
+
+import nltk
+nltk.download('averaged_perceptron_tagger_eng')
+
+# Speed is adjustable
+speed = 1.0
+
+# CPU is sufficient for real-time inference.
+# You can set it manually to 'cpu' or 'cuda' or 'cuda:0' or 'mps'
+device = 'auto' # Will automatically use GPU if available
+
+# English 
+model = TTS(language='EN', device=device)
+speaker_ids = model.hps.data.spk2id
 
 app = Flask(__name__)
 
