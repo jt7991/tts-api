d57e136b811b3dad19ceed078383bca041521d10
jt7991 <jt7991@gmail.com>
2025-01-09 22:42:56 -0500

    oauth url update

--- frontend/src/lib/server/auth/oauth.ts
+++ frontend/src/lib/server/auth/oauth.ts
@@ -1,8 +1,11 @@
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
 import { Google } from 'arctic';
 
 export const google = new Google(
-	env.GOOGLE_CLIENT_ID,
-	env.GOOGLE_CLIENT_SECRET,
-	'http://localhost:6969/login/google/callback'
+	env.GOOGLE_CLIENT_ID || '',
+	env.GOOGLE_CLIENT_SECRET || '',
+	dev
+		? 'http://localhost:6969/login/google/callback'
+		: 'http://tts-new.jakeystikiroom.com/login/google/callback'
 );
